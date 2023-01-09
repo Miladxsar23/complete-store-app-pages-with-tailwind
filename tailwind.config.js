6c3ede1cc047a7dbb77f83804950dc74d0e0a9c6
milad.xsar23 <milad.xsar72@gmail.com>
2023-01-09 17:23:12 +0330

    configurate our project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily : {
+        sans:['iranyekan']
+      }
+    },
+  },
+  plugins: [],
+}
